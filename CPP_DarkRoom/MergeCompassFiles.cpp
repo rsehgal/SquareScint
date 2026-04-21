d2b937bc568cc263f6873c92019cd21f7adf29b9
Raman Sehgal <sc.ramansehgal@gmail.com>
2026-04-21 13:58:10 +0530

    Now successfullly merging the files from CoMPASS

--- CPP_DarkRoom/MergeCompassFiles.cpp
+++ CPP_DarkRoom/MergeCompassFiles.cpp
@@ -9,38 +9,57 @@
 #include <TProfile.h>
 #include <fstream>
 #include <TGraph.h>
-int main(int argc, char *argv[])
-{
 
-  TFile *fout  = new TFile("combined_raw.root", "RECREATE");
-  TTree *ftree = nullptr; // new TTree("Data_F", "Combined files");
-  UShort_t Channel;
+std::vector<std::string> GetVecOfFiles(std::string runPrefix = "Capture_Data_BG",
+                                       std::string directory = "/home/muon/RemoteWork/DAQ/")
+{
+  std::vector<std::string> vecOfFiles;
+  for (unsigned int i = 0; i < 24; i++) {
+    std::string runPrefix2 = "";
+    if (i)
+      runPrefix2 += runPrefix + "_" + std::to_string(i);
+    else
+      runPrefix2 = runPrefix;
+    std::string filepath = directory + runPrefix2 + "/FILTERED/DataF_" + runPrefix2 + ".root";
+    // std::cout << filepath << std::endl;
+    vecOfFiles.push_back(filepath);
+  }
+  return vecOfFiles;
+}
+
+int main()
+{
+  TFile *fout = new TFile("combined_raw.root", "RECREATE");
+  fout->cd();
+  TTree *ftree = new TTree("Data_F", "Merged TTree"); // nullptr;
+
+  // Data variables
+  UShort_t Channel, Board, Energy, EnergyShort;
   ULong64_t Timestamp;
-  UShort_t Board;
-  UShort_t Energy;
-  UShort_t EnergyShort;
   UInt_t Flags;
 
+  ftree->Branch("Channel", &Channel);
+  ftree->Branch("Timestamp", &Timestamp);
+  ftree->Branch("Board", &Board);
+  ftree->Branch("Energy", &Energy);
+  ftree->Branch("EnergyShort", &EnergyShort);
+  ftree->Branch("Flags", &Flags);
+
   ULong64_t cumulativeOffset = 0;
-  ULong64_t maxTimestamp     = 0;
+  /*std::vector<const char *> filenameVec = {
+      "/home/user/DataF_CFD_Square1_Square2_LongRun_2.root",
+      "/home/user/DataF_CFD_Square1_Square2_LongRun_3.root"
+  };*/
 
-  // std::vector<char*>
-  // filenameVec={"DataF_CFD_Square_LongRun_23Mar.root","DataF_CFD_Square_LongRun_23Mar_1.root","DataF_CFD_Square_LongRun_23Mar_2.root","DataF_CFD_Square_LongRun_23Mar_3.root"};
+  std::vector<std::string> filenameVec = GetVecOfFiles();
+  for (auto fname : filenameVec) {
+    TFile *fp = TFile::Open(fname.c_str(), "READ");
+    if (!fp || fp->IsZombie()) {
+      std::cerr << "Error opening " << fname << std::endl;
+      continue;
+    }
 
-  std::vector<const char *> filenameVec = {"~/DataF_CFD_Square1_Square2_LongRun_2.root",
-                                           "~/DataF_CFD_Square1_Square2_LongRun_3.root"};
-  ULong64_t start                       = 0;
-
-  for (unsigned int fid = 0; fid < filenameVec.size(); fid++) {
-
-    maxTimestamp = 0;
-    std::cout << "@@@@ Processing File : " << filenameVec[fid] << " @@@@" << std::endl;
-    std::cout << "Cumulative Offset : " << cumulativeOffset << std::endl;
-
-    TFile *fp     = new TFile(filenameVec[fid]);
     TTree *Data_F = (TTree *)fp->Get("Data_F");
-
-    // Set branch addresses.
     Data_F->SetBranchAddress("Channel", &Channel);
     Data_F->SetBranchAddress("Timestamp", &Timestamp);
     Data_F->SetBranchAddress("Board", &Board);
@@ -48,26 +67,30 @@ int main(int argc, char *argv[])
     Data_F->SetBranchAddress("EnergyShort", &EnergyShort);
     Data_F->SetBranchAddress("Flags", &Flags);
 
-    if (!ftree) {
-      ftree = Data_F->CloneTree(0);
-      ftree->SetDirectory(fout); // Force the tree to live in the output file
-    }else{
-ftree->CopyAddresses(Data_F);
-}
+    ULong64_t maxTimestampInFile = 0;
+    Long64_t nentries            = Data_F->GetEntries();
 
-    Long64_t nentries = Data_F->GetEntries();
-
-    Long64_t nbytes = 0;
     for (Long64_t i = 0; i < nentries; i++) {
-      nbytes += Data_F->GetEntry(i);
-      if (Timestamp > maxTimestamp) maxTimestamp = Timestamp;
+      Data_F->GetEntry(i);
+
+      if (Timestamp > maxTimestampInFile) maxTimestampInFile = Timestamp;
+
+      // Apply offset
       Timestamp += cumulativeOffset;
       ftree->Fill();
     }
-    cumulativeOffset += maxTimestamp + 1;
+
+    std::cout << "Processed: " << fname << "\n Added offset: " << cumulativeOffset << std::endl;
+
+    // Update offset for next file
+    cumulativeOffset += (maxTimestampInFile + 1);
+
     fp->Close();
+    delete fp; // Clean up memory
   }
 
+  fout->cd();
   ftree->Write();
   fout->Close();
+  return 0;
 }
